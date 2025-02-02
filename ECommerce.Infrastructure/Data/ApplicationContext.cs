4b695413337ae3fb7c9e32855dfd379aa740e427
aliabooof <aliabooof42@gmail.com>
2025-02-02 16:38:01 +0200

    initialCommit

---
+++ ECommerce.Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using EcommerceSolution.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Data
+{
+    public class ApplicationContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ EcommerceSolution.Domain/Entities/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceSolution.Domain.Entities
+{
+    public class AppUser:IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; } = default!;
+        [Required]
+        public string LastName { get; set; } = default!;
+        [Required]
+        public DateTime BirthDate { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        public string ProfileImage { get; set; } = default!;
+    }
+}
